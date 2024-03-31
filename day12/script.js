06776e19a06566960604c0b306a9e7d9efb30951
Saymamulla2604 <108901313+Saymamulla2604@users.noreply.github.com>
2024-04-01 00:14:15 +0530

    Add files via upload

---
+++ day12/script.js
@@ -0,0 +1,32 @@
+
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    // to prevent values from being sent to the server
+    e.preventDefault();
+    const height = parseFloat(document.querySelector("#height").value);
+    const weight = parseFloat(document.querySelector("#weight").value);
+    const results = document.querySelector("#results");
+    const details = document.querySelector("#details");
+
+    if (height === "" || isNaN(height) || height <= 0) {
+        results.innerHTML = "Please provide a valid height";
+    } else if (height === "" || isNaN(weight) || weight <= 0) {
+        results.innerHTML = "Please provide a valid weight";
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        results.innerHTML = `<span>${bmi}</span>`;
+        // for underweight
+        if (bmi < 18.6) {
+            details.innerHTML = `<span>You are underweight</span>`;
+        }
+        // for normal weight
+        else if (bmi >= 18.6 && bmi <= 24.9) {
+            details.innerHTML = `<span>Your BMI is Normal</span>`;
+        }
+        // for overweight
+        else {
+            details.innerHTML = `<span>You are overweight</span>`;
+        }
+    }
+});
